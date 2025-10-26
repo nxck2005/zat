b11cc57973c2101fa4041bbefd896b247e0f328b
Nishchal Ravi <nxckmusicprod@gmail.com>
2025-10-27 01:24:00 +0530

    Add basic cat logic

--- src/root.zig
+++ src/root.zig
@@ -1,11 +1,45 @@
 const std = @import("std");
 
-pub fn printFile() !void {
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
-    const stdout = &stdout_writer.interface;
+// 4KB buffer
+const W_BUF: i32 = 4096;
+const R_BUF: i32 = 4096;
 
-    try stdout.print("File:", .{});
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    // get args
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    const arglen = args.len;
+    if (arglen != 2) {
+        std.debug.print("Usage: {s} <filename> \n", .{args[0]});
+        return;
+    }
+
+    var out_buf: [W_BUF]u8 = undefined;
+    var writer = std.fs.File.stdout().writer(&out_buf);
+    const stdout = &writer.interface;
+
+    var read_buf: [R_BUF]u8 = undefined;
+
+    const filename = args[1];
+    const file = try std.fs.cwd().openFile(filename, .{});
+    defer file.close();
+
+    var fr = file.reader(&read_buf);
+    const reader = &fr.interface;
+
+    while (true) {
+        const line = reader.takeDelimiterInclusive('\n') catch |err| {
+            if (err == error.EndOfStream) break;
+            return err;
+        };
+        try stdout.writeAll(line);
+    }
 
     try stdout.flush();
+    //std.debug.print("{} bytes written\n", .{n});
 }
