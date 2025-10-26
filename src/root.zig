801fa565761b2905a417e711827bac12a263a0ef
Nishchal Ravi <nxckmusicprod@gmail.com>
2025-10-27 02:30:29 +0530

    Change allocator, writer to writerStreaming and change loop to streamRemaining for syscall efficiency

--- src/root.zig
+++ src/root.zig
@@ -3,10 +3,9 @@ const std = @import("std");
 // 4KB buffer
 const W_BUF: usize = 4096;
 const R_BUF: usize = 4096;
-const C_BUF: usize = 4096;
 
-pub fn zat() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+pub fn main() !void {
+    var gpa = std.heap.DebugAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
@@ -21,27 +20,19 @@ pub fn zat() !void {
     }
 
     var out_buf: [W_BUF]u8 = undefined;
-    var writer = std.fs.File.stdout().writer(&out_buf);
+    var writer = std.fs.File.stdout().writerStreaming(&out_buf);
     const stdout = &writer.interface;
-
-    var read_buf: [R_BUF]u8 = undefined;
+    errdefer stdout.flush() catch {};
 
     const filename = args[1];
     const file = try std.fs.cwd().openFile(filename, .{});
     defer file.close();
 
+    var read_buf: [R_BUF]u8 = undefined;
     var fr = file.reader(&read_buf);
     const reader = &fr.interface;
 
-    var cbuf: [C_BUF]u8 = undefined;
-
-    while (true) {
-        const rb = reader.readSliceShort(&cbuf) catch |err| {
-            return err;
-        };
-        if (rb == 0) break;
-        try stdout.writeAll(read_buf[0..rb]);
-    }
+    _ = try reader.streamRemaining(stdout);
 
     try stdout.flush();
     //std.debug.print("{} bytes written\n", .{n});
