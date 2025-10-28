140c644a51c53381bebd705e73f86888d1b6a7d6
Nishchal Ravi <nxckmusicprod@gmail.com>
2025-10-29 00:53:50 +0530

    Added stdin read functionality

--- src/root.zig
+++ src/root.zig
@@ -14,26 +14,47 @@ pub fn zat() !void {
     defer std.process.argsFree(allocator, args);
 
     const arglen = args.len;
-    if (arglen != 2) {
-        std.debug.print("Usage: {s} <filename> \n", .{args[0]});
+    if (arglen < 2) {
+        // the stdin path
+        var out_buf: [W_BUF]u8 = undefined;
+        var writer = std.fs.File.stdout().writerStreaming(&out_buf);
+        const stdout = &writer.interface;
+        errdefer stdout.flush() catch {};
+
+        var in_buf: [R_BUF]u8 = undefined;
+        var in = std.fs.File.stdin().reader(&in_buf);
+        const in_reader = &in.interface;
+        while (true) {
+            while (in_reader.takeDelimiterExclusive('\n')) |line| {
+                try stdout.writeAll(line);
+                try stdout.writeAll("\n");
+                try stdout.flush();
+            } else |err| switch (err) {
+                error.EndOfStream, // stream ended not on a line break
+                error.StreamTooLong, // line could not fit in buffer
+                error.ReadFailed, // caller can check reader implementation for diagnostics
+                => |e| return e,
+            }
+        }
+    } else if (arglen == 2) {
+        var out_buf: [W_BUF]u8 = undefined;
+        var writer = std.fs.File.stdout().writerStreaming(&out_buf);
+        const stdout = &writer.interface;
+        errdefer stdout.flush() catch {};
+
+        const filename = args[1];
+        const file = try std.fs.cwd().openFile(filename, .{});
+        defer file.close();
+
+        var read_buf: [R_BUF]u8 = undefined;
+        var fr = file.reader(&read_buf);
+        const reader = &fr.interface;
+
+        _ = try reader.streamRemaining(stdout);
+
+        try stdout.flush();
+    } else {
+        std.debug.print("Usage: {s} <filename>", .{args[0]});
         return;
     }
-
-    var out_buf: [W_BUF]u8 = undefined;
-    var writer = std.fs.File.stdout().writerStreaming(&out_buf);
-    const stdout = &writer.interface;
-    errdefer stdout.flush() catch {};
-
-    const filename = args[1];
-    const file = try std.fs.cwd().openFile(filename, .{});
-    defer file.close();
-
-    var read_buf: [R_BUF]u8 = undefined;
-    var fr = file.reader(&read_buf);
-    const reader = &fr.interface;
-
-    _ = try reader.streamRemaining(stdout);
-
-    try stdout.flush();
-    //std.debug.print("{} bytes written\n", .{n});
 }
